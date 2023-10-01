90ac41bef8d50e7d25cc7aef1c4c94b42ab766ba
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-10-01 09:00:46 +0000

    enum 導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  
+  enum gender_method: { unentered: 0, male: 1, female: 2, others: 3 }
+  enum age_method: { unentered: 0, teens: 1, twenties: 2, thirties: 3, forties: 4, fifties: 5, sixties: 6, seventies: 7, eighties: 8 }
+  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
 end

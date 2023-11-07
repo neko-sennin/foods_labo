b4d1d71c7ca4bc926d2bfd2f77b9dab31f59ab0d
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-11-07 13:17:15 +0000

    タグ・カテゴリ一覧　view

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,7 +1,12 @@
 class Public::CategoriesController < ApplicationController
   
   def index
-    
+    @categories = Category.page(params[:page])
+    if params[:category_id].present?
+      #presentメソッドでparams[:category_id]に値が含まれているか確認 => trueの場合下記を実行
+      @category = Category.find(params[:category_id])
+      @foods = @category.foods
+    end
   end
   
 end

9631a92ddd6ce240c80a65b2285cdb5df7c217c4
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-11-09 12:59:13 +0000

    タグ・カテゴリ一覧　表示

--- app/controllers/public/foods_controller.rb
+++ app/controllers/public/foods_controller.rb
@@ -1,7 +1,7 @@
 class Public::FoodsController < ApplicationController
   
   def index
-    @foods = Food.page(params[:page])
+    @foods = Food.where(category_id: params[:category_id]).page(params[:page])
     @foods_all = Food.all
   end
 

4280ecce85ce7f08a4697b4e9ee68becd9c1bb1b
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-11-10 06:06:11 +0000

    会員退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,26 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  
+  protected
+  
+  def reject_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && (@user.is_active == true)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_user_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    else
+      flash[:notice] = "該当するユーザーが見つかりません"
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   # GET /resource/sign_in
   # def new

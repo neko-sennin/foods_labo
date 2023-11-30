329e51c39c5bcb6b05f7d7b85f2f8e96f43569c8
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-11-30 07:49:05 +0000

    お問い合せメール編集

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -20,8 +20,9 @@ class Public::ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.send_mail(@contact, current_user).deliver_now
-      redirect_to root_path
+      redirect_to root_path, flash: {success: "お問い合せが完了しました。"}
     else
+      flash.now[:danger] = "お問い合せ内容に不備があります。"
       render :new
     end
   end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,4 +6,16 @@ class ContactMailer < ApplicationMailer
     mail to:   user.email, subject: '【お問い合わせ】' + @contact.subject
   end
   
+  # def contact_mail(contact, user)
+  #   @contact = contact
+  #   mail to: user.email, bcc: ENV["ACTION_MAILER_USER"], subject: "お問い合わせについて【自動送信】"
+  # end
+  
+  # def send_mail(contact)
+  #   @contact = contact
+  #   mail(from: contact.email, to: ENV['MAIL_ADDRESS'], subject: 'Webサイトよりお問い合わせが届きました') do |format|
+  #     format.text
+  #   end
+  # end
+  
 end

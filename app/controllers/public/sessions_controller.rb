d8f3c39c4fbe630e28e26cc9921d82101614084d
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-12-01 03:48:19 +0000

    contact layout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -10,13 +10,13 @@ class Public::SessionsController < Devise::SessionsController
     @user = User.find_by(email: params[:user][:email])
     if @user
       if @user.valid_password?(params[:user][:password]) && (@user.is_active == true)
-        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        flash[:danger] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_user_registration_path
       else
-        flash[:notice] = "項目を入力してください"
+        flash[:danger] = "項目を入力してください"
       end
     else
-      flash[:notice] = "該当するユーザーが見つかりません"
+      flash[:danger] = "該当するユーザーが見つかりません"
     end
   end
 

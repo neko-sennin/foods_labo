6729f75dd8e6eb61724965abdbf90a85a8e09291
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2024-06-28 01:12:58 +0000

    日本時間訂正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module FoodsLabo
     config.load_defaults 6.1
     
     config.i18n.default_locale = :ja
+    
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -22,5 +24,3 @@ module FoodsLabo
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
-config.time_zone = 'Tokyo'

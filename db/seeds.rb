e710eb4a9c34eb9b039a4805689fee53aa865349
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2024-06-28 07:45:54 +0000

    日本語に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,25 @@ Admin.create!(
   password: 'testtest'
 )
 
+
 taro = User.find_or_create_by!(email: "user@user") do |user|
   user.name = "太郎"
   user.password = "password"
 end
+hanako = User.find_or_create_by!(email: "1@gmail.com") do |user|
+  user.name = "花子"
+  user.password = "password"
+end
+zirou = User.find_or_create_by!(email: "2@gmail.com") do |user|
+  user.name = "次郎"
+  user.password = "password"
+end
+
 
 banana = Food.find_or_create_by!(name: "バナナ", category_id: 1) do |food|
   
 end
+
 
 Review.find_or_create_by!(title: "ダイエットに効果的！") do |review|
   review.food = banana
@@ -36,4 +47,22 @@ categories = [
 
 categories.each do |category|
   Category.find_or_create_by(category)
+end
+
+
+tags = [
+  {id: 1, name: "ダイエット"},
+  {id: 2, name: "美肌"},
+  {id: 3, name: "美白"},
+  {id: 4, name: "健康維持"},
+  {id: 5, name: "免疫力"}
+  {id: 6, name: "ストレス緩和"}
+  {id: 7, name: "便秘(腸活)"}
+  {id: 8, name: "体脂肪"}
+  {id: 9, name: "血糖値"}
+  {id: 10, name: "睡眠の質向上"}
+]
+
+tags.each do |tag|
+  Tag.find_or_create_by(tag)
 end
